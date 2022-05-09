12ca083b4bb472915d9d30d972ad389ce11dc602
eskim_o <eunddo2001@naver.com>
2022-05-09 13:53:26 +0900

    c

--- 암호구현및실습/과제/AVL/AVL.c
+++ 암호구현및실습/과제/AVL/AVL.c
@@ -1,46 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
+#define max(a,b)  (((a) > (b)) ? (a) : (b))
+
+int BN[100]={0};
+int cnt;
 
 typedef struct TreeNode{
     int key;
+    int balance;
     struct TreeNode *left, *right;
 } TreeNode;
 
 TreeNode* new_node(int item){
     TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
     temp->key=item;
+    temp->balance=0;
     temp->left = NULL;
     temp->right=NULL;
     return temp;
 }
 
-TreeNode* insert_node(TreeNode **node, int key){
-    if(node ==NULL)
-        node = new_node(key);
-    
-    if(key<node->key)
-        (*node)->left = insert_node((*node)->left, key);
-    
-    else if(key>(*node)->key)
-        (*node)->right = insert_node((*node)->right, key);
+int get_height(TreeNode *node){
+    int height=0;
+    if(node!=NULL){
+        height = 1+max(get_height(node->left), get_height(node->right));
+    }
+    return height;
+}
 
-    return node;
+int get_balance(TreeNode *node){
+    if (node ==NULL)
+        return 0;
+    return get_height(node->left) - get_height(node->right);
+}
+
+TreeNode* insert_node(TreeNode **node, int key){
+    if(*node ==NULL)
+        (*node) = new_node(key);
+    
+    if(key<(*node)->key){
+        (*node)->left = insert_node(&((*node)->left), key);
+        (*node)->balance = get_balance(*node);
+        if (((*node)->balance)<-1 || ((*node)->balance)>1){
+            cnt++;
+            printf("cnt is %d\n", cnt);
+            BN[cnt]=(long int)((*node)->key);                
+            printf("left %ld\n", (long int)((*node)->key));
+        }
+    }
+
+    else if(key>(*node)->key){
+        (*node)->right = insert_node(&((*node)->right), key);
+        (*node)->balance = get_balance(*node);
+        if (((*node)->balance)<-1 || ((*node)->balance)>1){
+            cnt++;
+            printf("cnt is %d\n", cnt);
+            BN[cnt]=(long int)((*node)->key);
+            printf("right %ld\n", (long int)((*node)->key));
+        }
+    }
+
+    return *node;
 }
 
 void inorder(FILE *stream, TreeNode *root){
     if(root){
         inorder(stream, root->left);
-        printf("stream_int : %p\n", stream);
-        fprintf(stream, "%d", root->key);
+        fprintf(stream, "%d ", root->key);
         inorder(stream, root->right);
     }
 }
 
 void preorder(FILE *stream, TreeNode *root){
-    printf("stream : %p", stream);
     if(root){
-        fprintf(stream, "%d", root->key);
+        fprintf(stream, "%d ", root->key);
         preorder(stream, root->left);
         preorder(stream, root->right);
     }
@@ -56,15 +90,20 @@ int main(void)
     TreeNode *root=NULL;
 
     while(fscanf(fp, "%s%d", op, &input) != EOF){
-        printf("stream : %p\n", fout);
-        fprintf(fout, "input: %d", input);
+        fprintf(fout, "input: %d\n", input);
+        printf("\ninput %d\n", input);
         insert_node(&root, input);
+
+        fprintf(fout, "I ");
         inorder(fout, root);
-        //preorder(fout, root);
         fprintf(fout, "\n");
-        fprintf(fout, "BN");
+
+        fprintf(fout, "P ");
+        preorder(fout, root);
         fprintf(fout, "\n");
-        fprintf(fout, "\n");
+
+        fprintf(fout, "BN %d\n\n", BN[1]);
+        cnt=0;
     }
     
     fclose(fp);
